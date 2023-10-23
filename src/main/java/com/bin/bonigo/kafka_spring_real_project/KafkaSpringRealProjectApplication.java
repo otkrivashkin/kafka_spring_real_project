111ea4bb0105cae70353a686a319b596a2c8154a
Ihor Bondarenko <ibondarenko@codifire.com>
2023-10-23 15:26:45 +0300

    Initial commit

---
+++ src/main/java/com/bin/bonigo/kafka_spring_real_project/KafkaSpringRealProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bin.bonigo.kafka_spring_real_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringRealProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSpringRealProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bin/bonigo/kafka_spring_real_project/KafkaSpringRealProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bin.bonigo.kafka_spring_real_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringRealProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
